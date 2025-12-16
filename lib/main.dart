04b95e7eb46d66a49d3dec13517611c251dd1f39
Marah Saadeh <marah4saadeh@gmail.com.com>
2025-12-16 19:18:54 +0200

    Initial commit - Full Flutter project with responsive design
    - Added MobileScreen with CupertinoTabBar navigation
    - Home, Search, AddPost, Favorites, Profile
    - PageView controlled by bottom navigation
    - Added WebScreen for desktop layout
    - Added Responsive widget to switch between MobileScreen and WebScreen
    - Created individual screens: Home, Search, AddPost, Profile

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
-import 'package:social_app/home.dart';
+import 'package:social_app/responsive/mobile.dart';
+import 'package:social_app/responsive/responsive.dart';
+import 'package:social_app/responsive/web.dart';
+
 void main() {
   runApp(const MyApp());
 }
- 
+
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
- 
+
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Home(),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      home: Resposive(
+        myMobileScreen: MobileScerren(),
+        myWebScreen: WebScerren(),
+      ),
     );
   }
 }
---
+++ lib/responsive/web.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class WebScerren extends StatefulWidget {
+  const WebScerren({Key? key}) : super(key: key);
+
+  @override
+  State<WebScerren> createState() => _WebScerrenState();
+}
+
+class _WebScerrenState extends State<WebScerren> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("web screen"),
+      ),
+    );
+  }
+}
