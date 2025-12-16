abd687f8db0280c3bcaebfec275716a90ba747fc
Marah Saadeh <marah4saadeh@gmail.com.com>
2025-12-16 16:46:55 +0200

    first commit

---
+++ lib/home.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final double widthScreen = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: widthScreen > 600
+            ? Text("web")
+            : Text("Mobile Screen"),
+      ),
+    );
+  }
+}
